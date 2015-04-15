e661fabd538ff0af57886ea799c158e2950d062c
wayne <wayne@index.com>
2015-04-14 20:14:12 -0700

    copied src files to dist

---
+++ dist/angularChartist.js
@@ -0,0 +1,5 @@
+// (WAM): code in this module is based on the angular-chartist wrapper library
+// (version 3.1.0) found here: https://github.com/paradox41/angular-chartist.js
+// and has been modified to conform to our general code practices
+
+angular.module('angular-chartist', []);
